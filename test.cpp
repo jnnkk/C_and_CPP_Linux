7fda621da170d581ca5d023417fa999c968ef48c
SJY <jun000405@naver.com>
2023-09-13 10:24:21 +0900

    3주차 실습

--- test.cpp
+++ test.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <ctime>
 
-int main()
-{
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+using namespace std;
+
+int odd(int x) {
+	return (x % 2);
+}
+
+inline int odd_inline(int x) {
+	return (x % 2);
+}
+
+int main() {
+	int itr = 1000000;
+	int cnt = 0;
+	clock_t s, e;
+
+	s = clock();
+	for (int i = 1; i <= itr; i++) {
+		if (odd(i))
+			cnt++;
+	}
+	cout << cnt << endl;
+	e = clock();
+
+	cout << "normal:" << (double)(e - s) / CLOCKS_PER_SEC << endl;
+
+	cnt = 0;
+	s = clock();
+	for (int i = 1; i <= itr; i++) {
+		if (odd_inline(i)) {
+			cnt++;
+		}
+	}
+	cout << cnt << endl;
+	e = clock();
+
+	cout << "inline:" << (double)(e - s) / CLOCKS_PER_SEC << endl;
+
+	return 0;
 }
