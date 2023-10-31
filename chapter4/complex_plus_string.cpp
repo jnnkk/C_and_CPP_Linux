f532c3b8b2cccbadfcbb3fae07bbd3d4169efce0
SJY <jun000405@naver.com>
2023-10-31 12:36:23 +0900

    fix: 파일이름에 () 있으면 디버깅 안됨

--- chapter4/complex_plus_string.cpp
+++ chapter4/complex_plus_string.cpp
@@ -5,16 +5,16 @@ using namespace std;
 
 int main() {
     string a;
-    int index = 0;
+    size_t index = 0;
     int sum = 0;
 
     cout << "7+23+5+100+25와 같이 덧셈 문자열을 입력하세요. " << endl;
     getline(cin, a); // 공백입력을 위해 getline 사용
 
-    while (index != -1) {
-        index = a.find('+', index);
+    while ((index = a.find('+')) != string::npos) {
         sum += stoi(a.substr(0, index));
         a = a.substr(index + 1);
     }
-    cout << sum + stoi(a) << endl;
+    sum += stoi(a);
+    cout << sum << endl;
 }
